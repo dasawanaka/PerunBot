dc3dc2fc65d4e201404adf4a187f1c7198bbaf5e
Maliniara <84290119+Maliniara@users.noreply.github.com>
2021-05-20 20:56:02 +0200

    Fixed messageDeleted.js
    Signed-off-by: Maliniara <84290119+Maliniara@users.noreply.github.com>

--- events/message/messageDelete.js
+++ events/message/messageDelete.js
@@ -1,35 +1,29 @@
 const { MessageEmbed } = require("discord.js");
-const { Logger } = require("mongodb");
 const LOGGER = require("../../class/logger");
 
 module.exports = {
   name: "messageDelete",
   description: "this is event for delete message",
   async run(message, client) {
+    if (message.partial) await message.fetch();
+    
     const { content, channel, author, guild, mentions } = message;
-    if (
-      !author ||
-      author.bot ||
-      channel.type === "dm"
-    ) {
+    
+    if (!author || author.bot || channel.type === "dm") {
       return;
     }
-    if (
-      
-      mentions.users.size != 0
 
-    ) {
+    if (mentions.users.size === 0) {
       let embed = new MessageEmbed()
-      .setTitle("Message Deleted")
-      .setDescription(`Message content:\n${content}`)
-      .addField("Channel", channel)
-      .addField("Mentions count", `${mentions.users.size}`)
-      .addField("Message author", author)
-      .setColor("#f55742");
-    
-      LOGGER.log(guild, embed)
-    }
+        .setTitle("Message Deleted")
+        .setDescription(`Message content:\n${content}`)
+        .addField("Channel", channel)
+        .addField("Message author", author)
+        .setColor("#f55742");
 
+      LOGGER.log(guild, embed)
+      return;
+    }
 
     let embed = new MessageEmbed()
       .setTitle("Possible ghost ping detected")
