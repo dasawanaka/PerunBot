021c219ca5ecccbbdcb48e6054cdee0729d224cd
dasawanaka <michal.19.daniel.96@gmail.com>
2021-06-21 21:23:16 +0200

    MORE FUN WITH TICKETS!!!
    Signed-off-by: dasawanaka <michal.19.daniel.96@gmail.com>

--- commands/mod/tickets/archive.js
+++ commands/mod/tickets/archive.js
@@ -1,5 +1,9 @@
 const Command = require("../../../assets/class/Command");
 const fetchAll = require("discord-fetch-all");
+var pdf = require("pdf-creator-node");
+var fs = require("fs");
+const path = require("path");
+const { MessageAttachment } = require("discord.js");
 
 class Archive extends Command {
   constructor() {
@@ -9,8 +13,11 @@ class Archive extends Command {
     });
   }
   async run(client, message, args) {
-    // First parameter needs to be a discord.js channel object
-    // Second parameter is a optional set of options.
+    const channelName = message.channel.name;
+    if (!channelName.startsWith("ticket")) return;
+    const channelArgs = channelName.split("-");
+    const ticketCreatorId = channelArgs[channelArgs.length - 1];
+
     const allMessages = await fetchAll.messages(message.channel, {
       reverseArray: true, // Reverse the returned array
       userOnly: true, // Only return messages by users
@@ -18,10 +25,63 @@ class Archive extends Command {
       pinnedOnly: false, // Only returned pinned messages
     });
 
-    // Will return an array of all messages in the channel
-    // If the channel has no messages it will return an empty array
-    console.log(allMessages);
     console.log(allMessages.length);
+    const messagesToTemplate = [];
+    for (let index = 0; index < allMessages.length; index++) {
+      const m = allMessages[index];
+      var av = m.author.avatarURL({format: 'png', size:512});
+      if(!av){
+        av = m.author.defaultAvatarURL;
+      }
+      var messageToPush = {
+        author: m.author.tag,
+        content: m.content,
+        avatar: av,
+        date: m.createdAt,
+        side: m.author.id === ticketCreatorId?"right":"left",
+      };
+      messagesToTemplate.push(messageToPush);
+    }
+
+    //console.log(messagesToTemplate);
+
+    var html = fs.readFileSync(path.join(__dirname, "template.html"), "utf8");
+    var options = {
+      format: "A4",
+      orientation: "portrait",
+      border: "5mm",
+    };
+    const tmpFilePath = path.join(
+      __dirname,
+      "tmp",
+      `${message.channel.name}_${message.id}.pdf`
+    );
+    var document = {
+      html: html,
+      data: {
+        messages: messagesToTemplate,
+        channelName: channelName
+      },
+      path: tmpFilePath,
+      type: "",
+    };
+    await pdf
+      .create(document, options)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    const attachment = new MessageAttachment(
+      tmpFilePath,
+      path.basename(tmpFilePath)
+    );
+    await message.channel.send(attachment);
+
+    fs.unlink(tmpFilePath, (error) => {
+      if (error) client.logger.error(error);
+    });
   }
 }
 
